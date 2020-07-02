5beeb7d59ec64d519f50f7eba2c978c3fbf9313e
garybang <65151702+garybang@users.noreply.github.com>
2020-07-02 21:04:36 +0900

    20200702 배포용

--- NGGM/Crawling.py
+++ NGGM/Crawling.py
@@ -2,7 +2,7 @@ import requests, re
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient('mongodb://admin_gary:bang9476141!@13.125.251.5', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbEx
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
--- NGGM/app.py
+++ NGGM/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient('mongodb://admin_gary:bang9476141!@localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbEx                     # 'dbEx'라는 이름의 db를 만듭니다.
 
 @app.route('/')
